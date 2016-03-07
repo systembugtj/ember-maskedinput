2db08bb4b31fbee09fb7832e83535c24a6ad6ce5
Albert Li <albert_lee@hotmail.com>
2016-03-07 11:20:12 -0800

    call this._super in didInsertElement

--- addon/components/masked-input.js
+++ addon/components/masked-input.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.TextField.extend({
-  
   mask: "9999 9999",
   tagName: "input",
   maskholder: "-",
   autoclear: false,
   
   didInsertElement() {
+    this._super(...arguments);
     this.$().mask(this.get("mask"), { 
       placeholder: this.get("maskholder"),
       autoclear: this.get("autoclear"),
